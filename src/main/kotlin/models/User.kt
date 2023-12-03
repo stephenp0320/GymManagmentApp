901c889747929a1f0b61594da0c0201353d857c7
stephenp0320 <stephenpower03@gmail.com>
2023-12-03 17:58:25 +0000

    ADD KDoc to user

--- src/main/kotlin/models/User.kt
+++ src/main/kotlin/models/User.kt
@@ -1,5 +1,13 @@
 package models
 
+/**
+ * Represents a user in the system.
+ *
+ * @property userID The unique identifier for the user.
+ * @property userName The name of the user.
+ * @property userEmail The email address of the user.
+ * @property userPassword The password of the user (stored in plain text for simplicity, though not recommended in a real-world scenario).
+ */
 class User(
     var userID: Int,
     var userName: String,
@@ -7,6 +15,12 @@ class User(
     var userPassword: String
 ) {
 
+
+    /**
+     * Returns a string representation of the User object.
+     *
+     * @return A string that contains the user's ID, name, and email.
+     */
     override fun toString(): String {
         return "User ID: $userID, Name: $userName, Email: $userEmail"
     }
