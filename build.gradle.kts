480368d7d588bdd02c1994986b7232b2bc5186b0
stephenp0320 <stephenpower03@gmail.com>
2023-12-03 12:37:55 +0000

    ADD Dokka and KDoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
+    // Plugin for Dokka - KDoc generating tool
+    id("org.jetbrains.dokka") version "1.9.10"
     application
 }
 
@@ -11,12 +13,17 @@ repositories {
 }
 
 dependencies {
+   // testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.10")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.10")
     // dependencies for logging
     implementation("io.github.microutils:kotlin-logging:3.0.5")
     implementation("org.slf4j:slf4j-simple:2.0.9")
     implementation("com.thoughtworks.xstream:xstream:1.4.20")
     implementation("org.codehaus.jettison:jettison:1.5.4")
+
+
+    // For generating a Dokka Site from KDoc
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
 
 
 }
