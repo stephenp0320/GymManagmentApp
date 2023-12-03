5c682b9077ec1baae9a215947897e1e102f9365f
stephenp0320 <stephenpower03@gmail.com>
2023-12-03 16:09:00 +0000

    ADD tests for crud functionality for workoutAPI

--- src/test/kotlin/controllers/WorkoutAPITest.kt
+++ src/test/kotlin/controllers/WorkoutAPITest.kt
@@ -2,7 +2,7 @@ package controllers
 
 import models.Workout
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import persistence.JSONSerializer
@@ -26,7 +26,7 @@ class WorkoutAPITest {
         legs = Workout(4, "legs", "hypertrophy", 17, 70, false)
         cycling = Workout(5, "cycling", "cardio", 20, 10, true)
         treadmill = Workout(6, "treadmill", "cardio", 12, 90, false)
-        // adding 5 Note to the notes api
+
         populatedWorkouts!!.addWorkout(yoga!!)
         populatedWorkouts!!.addWorkout(back!!)
         populatedWorkouts!!.addWorkout(legs!!)
@@ -64,4 +64,106 @@ class WorkoutAPITest {
             assertEquals(storingNotes.numberOfWorkouts(), loadedNotes.numberOfWorkouts())
         }
     }
+
+
+    @Nested
+    inner class AddWorkout {
+        @org.junit.jupiter.api.Test
+        fun `adding a workout to a populated list adds to ArrayList`() {
+            val newWorkout = Workout(12, "legs", "Hypertrophy", 20, 70, false)
+            assertEquals(5, populatedWorkouts!!.numberOfWorkouts())
+            assertTrue(populatedWorkouts!!.addWorkout(newWorkout))
+            assertEquals(6, populatedWorkouts!!.numberOfWorkouts())
+            assertEquals(newWorkout, populatedWorkouts!!.findWorkout(populatedWorkouts!!.numberOfWorkouts() - 1))
+        }
+
+        @org.junit.jupiter.api.Test
+        fun `adding a workout to an empty list adds to ArrayList`() {
+            val newWorkout = Workout(12, "legs", "Hypertrophy", 20, 70, false)
+            assertEquals(0, emptyWorkouts!!.numberOfWorkouts())
+            assertTrue(emptyWorkouts!!.addWorkout(newWorkout))
+            assertEquals(1, emptyWorkouts!!.numberOfWorkouts())
+            assertEquals(newWorkout, emptyWorkouts!!.findWorkout(emptyWorkouts!!.numberOfWorkouts() - 1))
+        }
+    }
+
+
+    @Nested
+    inner class ListNotes {
+
+        @org.junit.jupiter.api.Test
+        fun `listAllWorkouts returns No workouts Stored message when ArrayList is empty`() {
+            assertEquals(0, emptyWorkouts!!.numberOfWorkouts())
+            assertFalse(emptyWorkouts!!.listAllWorkouts().lowercase().contains("no workouts"))
+        }
+
+
+    }
+
+    @Nested
+    inner class DeleteWorkout {
+
+        @org.junit.jupiter.api.Test
+        fun `deleting a workout that does not exist, returns null`() {
+            assertNull(emptyWorkouts!!.deleteWorkout(0))
+            assertNull(populatedWorkouts!!.deleteWorkout(-1))
+            assertNull(populatedWorkouts!!.deleteWorkout(5))
+        }
+
+        @org.junit.jupiter.api.Test
+        fun `deleting a workout that exists delete and returns deleted object`() {
+            assertEquals(5, populatedWorkouts!!.numberOfWorkouts())
+            assertEquals(legs, populatedWorkouts!!.deleteWorkout(2))
+            assertEquals(4, populatedWorkouts!!.numberOfWorkouts())
+            assertEquals(treadmill, populatedWorkouts!!.deleteWorkout(3))
+            assertEquals(3, populatedWorkouts!!.numberOfWorkouts())
+        }
+    }
+
+    @Nested
+    inner class UpdateWorkout {
+        @org.junit.jupiter.api.Test
+        fun `updating a Workout that does not exist returns false`() {
+            assertFalse(
+                populatedWorkouts!!.updateWorkout(
+                    6,
+                    Workout(12, "legs", "Hypertrophy", 20, 70, false)
+                )
+            )
+            assertFalse(
+                populatedWorkouts!!.updateWorkout(
+                    -1,
+                    Workout(12, "legs", "Hypertrophy", 20, 70, false)
+                )
+            )
+            assertFalse(
+                emptyWorkouts!!.updateWorkout(
+                    0, Workout(12, "legs", "Hypertrophy", 20, 70, false)
+                )
+            )
+        }
+
+        @org.junit.jupiter.api.Test
+        fun `updating a workout that exists returns true and updates`() {
+            // Assuming 'legs' is at index 2
+            assertEquals(legs, populatedWorkouts!!.findWorkout(2))
+            assertEquals("legs", populatedWorkouts!!.findWorkout(2)!!.workoutName)
+            assertEquals(4, populatedWorkouts!!.findWorkout(2)!!.workoutID)
+            assertEquals("hypertrophy", populatedWorkouts!!.findWorkout(2)!!.sessionType) // Original state
+
+            // Update the workout at index 2
+            assertTrue(
+                populatedWorkouts!!.updateWorkout(
+                    2,
+                    Workout(12, "legs", "Hypertrophy", 20, 70, false)
+                )
+            )
+            // Check if the workout at index 2 is updated correctly
+            assertEquals("legs", populatedWorkouts!!.findWorkout(2)!!.workoutName)
+            assertEquals(12, populatedWorkouts!!.findWorkout(2)!!.workoutID) // Updated ID
+            assertEquals("Hypertrophy", populatedWorkouts!!.findWorkout(2)!!.sessionType) // Updated session type
+        }
+    }
 }
+
+
